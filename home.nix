b33b0f4e24da4eec7536705f9bff28957fdae964
Victor Araque Casaus <Vctor Araque Casaus>
2024-06-06 15:51:23 +0200

    initial commit

---
+++ home.nix
@@ -0,0 +1,27 @@
+{ config, pkgs, ... }:
+
+{
+  # Home Manager needs a bit of information about you and the paths it should
+  # manage.
+  home.username = "soka";
+  home.homeDirectory = "/home/soka";
+
+  home.sessionVariables = {
+    EDITOR = "nvim";
+  };
+
+  programs.ssh.addKeysToAgent = "yes";
+
+  # The home.packages option allows you to install Nix packages into your
+  # environment.
+  home.packages = [
+  ];
+
+  home.file = {
+  };
+
+  home.stateVersion = "24.05"; # Please read the comment before changing.
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
